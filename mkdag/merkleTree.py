b245cce36900aa97cda93c307bb425cc28d03dc4
pritchardn <nicholas.pritchard@research.uwa.edu.au>
2020-03-18 12:18:08 +0800

    Renames MerkleTree Class Separates Block into separate file Adds basic test cases for add_data

--- mkdag/merkleTree.py
+++ mkdag/merkleTree.py
@@ -2,38 +2,10 @@
 # Released under Apache 2.0 License
 # Tested with 64-bit Python 3.8
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
+from Block import Block
 
 
-class Block(object):
-    def __init__(self):
-        self.data = {}
-        self.data_serial = None
-        self.digest = None
-        self.hash_algorithm = hashes.SHA256()
-        self.hash = None
-
-    def add_data(self, value, key="data"):
-        self.data[key] = repr(value)
-
-    def get_data(self):
-        return self.data
-
-    def generate_hash(self):
-        self.digest = hashes.Hash(self.hash_algorithm, backend=default_backend())
-        self.data_serial = repr(self.data)  # json.dumps(self.data, sort_keys=True)
-        self.digest.update(self.data_serial.encode(encoding="utf-8"))
-        self.hash = self.digest.finalize()
-        self.digest = None
-
-    def print(self):
-        for element in self.data:
-            print(str(element) + " " + str(self.data.get(element)))
-        print(self.hash)
-
-
-class Tree:
+class MerkleTree:
 
     def __init__(self):
         self.left = None
@@ -42,9 +14,9 @@ class Tree:
 
     def print(self):
         self.data.print()
-        if type(self.left) == Tree:
+        if type(self.left) == MerkleTree:
             self.left.print()
-        if type(self.right) == Tree:
+        if type(self.right) == MerkleTree:
             self.right.print()
 
     def _add_data(self, data):
@@ -54,9 +26,9 @@ class Tree:
             bound = len(data)//2
             if len(data) % 2 != 0:
                 bound += 1
-            self.left = Tree()
+            self.left = MerkleTree()
             self.data.add_data(self.left._add_data(data[:bound]), "left")
-            self.right = Tree()
+            self.right = MerkleTree()
             self.data.add_data(self.right._add_data(data[bound:]), "right")
         self.data.generate_hash()
         return self.data.hash
@@ -66,6 +38,6 @@ class Tree:
         self._add_data(data)
 
 
-mytree = Tree()
+mytree = MerkleTree()
 mytree.add_data(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
 mytree.data.generate_hash()
