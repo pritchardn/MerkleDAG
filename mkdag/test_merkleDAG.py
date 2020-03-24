374225602e2e8f092a5bd11a79c2c9e203716235
pritchardn <nicholas.pritchard@research.uwa.edu.au>
2020-03-24 16:02:01 +0800

    Adds a variety of merkleDag tests

--- mkdag/test_merkleDAG.py
+++ mkdag/test_merkleDAG.py
@@ -1,19 +1,50 @@
+from merkleDAG import MerkleDAG
+
+
 class TestMerkleDAG:
+    x = MerkleDAG()
+    y = MerkleDAG()
+    z = MerkleDAG()
+
+    def reset_class_instances(self):
+        self.x = MerkleDAG()
+        self.y = MerkleDAG()
+        self.z = MerkleDAG()
 
     def test_get_hash(self):
-        assert False
+        assert self.x.get_hash() is None
+        self.x.add_data(['A', 'B', 'C'])
+        assert self.x.get_hash() is not None
 
     def test_add_child(self):
-        assert False
-
-    def test_add_sibling(self):
-        assert False
+        self.x.add_child(self.y)
+        self.y.add_child(self.z)
+        assert self.x.parents == []
+        assert self.x in self.y.parents
+        assert self.y in self.z.parents
+        self.reset_class_instances()
 
     def test_add_parent(self):
-        assert False
-
-    def test_add_data(self):
-        assert False
+        self.z.add_parent(self.x)
+        self.z.add_parent(self.y)
+        self.y.add_parent(self.x)
+        assert self.x.parents == []
+        assert self.x in self.y.parents
+        assert self.x in self.z.parents and self.y in self.z.parents
+        self.reset_class_instances()
 
     def test_update_hashes(self):
-        assert False
+        self.x.add_data(['A', 'B', 'C'])
+        old_hash = self.x.get_hash()
+        self.x.add_child(self.y)
+        assert old_hash == self.x.get_hash()
+
+        old_hash = self.x.get_hash()
+        self.y.add_data(['D', 'E', 'F'])
+        self.x.update_hashes()
+        assert old_hash != self.x.get_hash()
+
+        old_hash = self.z.get_hash()
+        self.z.add_child(self.y)
+        self.z.update_hashes()
+        assert old_hash != self.z.get_hash()
