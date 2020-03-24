e16dc82ce80ba37357283be9ed270d4e2a6011f6
pritchardn <nicholas.pritchard@research.uwa.edu.au>
2020-03-24 14:12:14 +0800

    Adds basic relationship constructors

--- mkdag/merkleDAG.py
+++ mkdag/merkleDAG.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2020 N.J. Pritchard
+# Released under Apache 2.0 License
+# Tested with 64-bit Python 3.8
 from Block import *
 
 
@@ -14,3 +17,21 @@ class MerkleDAG(object):
         self.data.print()
         for i in self.children:
             i.print()
+
+    def add_child(self, child):
+        if type(child) == MerkleDAG:
+            if child not in self.children:
+                self.children.append(child)
+                child.add_parent(self)
+
+    def add_sibling(self, sibling):
+        if type(sibling) == MerkleDAG:
+            if sibling not in self.siblings:
+                self.siblings.append(sibling)
+                sibling.add_sibling(self)
+
+    def add_parent(self, parent):
+        if type(parent) == MerkleDAG:
+            if parent not in self.parents:
+                self.parents.append(parent)
+                parent.add_child(self)
