66fdfeb993a72e5740e76918084a58e7e183d180
pritchardn <nicholas.pritchard@research.uwa.edu.au>
2020-03-24 15:23:52 +0800

    Adds empty test_handler

---
+++ mkdag/test_merkleDAG.py
@@ -0,0 +1,19 @@
+class TestMerkleDAG:
+
+    def test_get_hash(self):
+        assert False
+
+    def test_add_child(self):
+        assert False
+
+    def test_add_sibling(self):
+        assert False
+
+    def test_add_parent(self):
+        assert False
+
+    def test_add_data(self):
+        assert False
+
+    def test_update_hashes(self):
+        assert False
