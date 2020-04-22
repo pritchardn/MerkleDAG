5e26d3aa5f2fae1ca81d93f2bf35c71b69e20cd2
pritchardn <21726929@student.uwa.edu.au>
2020-04-22 11:29:52 +0800

    Adds comments and type-checking to MerkleTree.py

--- MerkleDAG/MerkleTree.py
+++ MerkleDAG/MerkleTree.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2020 N.J. Pritchard
-# Released under Apache 2.0 License
-# Tested with 64-bit Python 3.8
 from Node import Node
 
 
 class MerkleTree:
+    """
+    Basic implementation of a MerkleTree
+    """
 
     def __init__(self):
         self.left = None
@@ -13,14 +14,26 @@ class MerkleTree:
         self.data = Node()
 
     def print(self):
+        """
+        Prints a representation of the MerkleTree
+        :return:
+        """
         self.data.print()
         if type(self.left) == MerkleTree:
             self.left.print()
         if type(self.right) == MerkleTree:
             self.right.print()
 
-    def _add_data(self, data):
+    def _add_data(self, data: list):
+        """
+        Internal recursive data adding routine
+        Splits supplied data into halves at each stages building the MerkleTree with recursive hashing
+        :param data: List of JSON-friendly elements
+        :return hash: The resulting hash of the current list
+        """
         print(data)
+        if type(data) != list:
+            raise TypeError("Data must be supplied in list form")
         self.data.add_data(data)
         if len(data) > 1:
             bound = len(data) // 2
@@ -38,11 +51,25 @@ class MerkleTree:
         return self.data.hash
 
     def add_data(self, data):  # Assumes data is list
+        """
+        Attempts to add the supplied data to the tree by splitting on a per-item basis.
+        Expects data to be serialized by JSON and in a list
+        :param data: List of JSON-friendly elements
+        :return hash: A string representation of the resulting root hash
+        """
         print(data)
-        self._add_data(data)
+        return self._add_data(data)
 
 
 def compare_mktree(x: MerkleTree, y: MerkleTree):
+    """
+    Compares two MerkleTrees using the minimal number of comparisons
+    :param x: The first MerkleTree
+    :param y: The second MerkleTree
+    :return: True if the trees are identical, false otherwise
+    """
+    if type(x) != MerkleTree or type(y) != MerkleTree:
+        raise TypeError("Must compare MerkleTrees")
     if x is None and y is None:
         return True
     if x is not None and y is not None:
